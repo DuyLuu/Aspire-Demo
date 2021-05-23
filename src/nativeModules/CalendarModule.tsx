7cb406385a84603968bf9406358793286f33c675
DuyLuu <bdl.uit@gmail.com>
2021-05-23 11:16:15 +0700

    Remove redundant code

--- src/nativeModules/CalendarModule.tsx
+++ src/nativeModules/CalendarModule.tsx
@@ -1,10 +1,7 @@
 import { NativeModules } from 'react-native'
 const { CalendarModule } = NativeModules
 
-const { DEFAULT_EVENT_NAME } = CalendarModule.getConstants()
-
 interface CalendarInterface {
     createCalendarEvent(name: string, location: string): void;
 }
-console.log('=== DEFAULT_EVENT_NAME', DEFAULT_EVENT_NAME)
 export default CalendarModule as CalendarInterface
